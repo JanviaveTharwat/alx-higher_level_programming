6f2553e46ab98188d206896987910dd320ca0932
JanviaveTharwat <janviavetharwat@gmail.com>
2023-06-06 15:35:00 -0700

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-            print("{}".format(i), end="")
-            print()
+    i = 0
+    news = ""
+    while i != len(str):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            new += chr(ord(str[i]) - 32)
+        else:
+            news += str[i]
+            i += 1
+            print("{}".format(news))
