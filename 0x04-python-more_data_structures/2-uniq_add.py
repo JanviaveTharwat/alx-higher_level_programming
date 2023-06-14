ba95a765ae82748ddaceb0d0140a7f7b52d80dc2
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-06-14 20:35:17 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-        return add
+    new_list = []
+    sum = 0
+    for num in my_list:
+        if num not in new_list:
+            sum += num
+            new_list.append(num)
+            return sum
