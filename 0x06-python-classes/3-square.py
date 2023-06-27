cba467cd3eabe2418d03656f3059ef1767bec722
janviave <janviavetharwat@gmail.com>
2023-06-27 10:06:44 -0700

    ;

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+""" defines a square """
 
 
 class Square:
-    """ class Square that defines a square"""
+    """ square with private instance attribute size """
 
     def __init__(self, size=0):
-        """initialize square
-        Args:
-        size (int): size of the square
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+        initializes square
+        Args:
+        size: size of side of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+            else:
+                raise TypeError('size must be an integer')
 
             def area(self):
-                """returns the area.
-
-                Returns:
-                ares.
                 """
-                return self.__size**2
+                finds area of square
+                Returns:
+                the area of the square
+                """
+
+                return self.__size ** 2
