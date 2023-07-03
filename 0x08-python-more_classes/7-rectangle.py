b9ca310502a54d6e5948f8ee5976286bf293f394
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-07-03 18:38:57 +0200

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-        
+  
         Args:
         width (int): The width of the new rectangle.
         height (int): The height of the new rectangle.
@@ -48,8 +48,8 @@ class Rectangle:
                 def height(self, value):
                     if not isinstance(value, int):
                         raise TypeError("height must be an integer")
-                   if value < 0:
-                       raise ValueError("height must be >= 0")
+                    if value < 0:
+                        raise ValueError("height must be >= 0")
             self.__height = value
 
             def area(self):
