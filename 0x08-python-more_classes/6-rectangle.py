354148c1f7c89c91897a24194faa926da1fc5802
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-07-03 18:13:11 +0200

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -60,8 +60,8 @@ class Rectangle:
             """
             if self.__width == 0 or self.__height == 0:
                 return (0)
-            return ((self.__width * 2) + (self.__height * 2))     
-
+            return ((self.__width * 2) + (self.__height * 2))
+ 
         def __str__(self):
             """
             Return the printable representation of the Rectangle.
