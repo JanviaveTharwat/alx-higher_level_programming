68cedd08664b3c0b73a6d8244c723bb09b37a1f5
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-07-03 18:43:11 +0200

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
- 
+
         Args:
         width (int): The width of the new rectangle.
         height (int): The height of the new rectangle.
@@ -73,12 +73,12 @@ class Rectangle:
                     """
                 if self.__width == 0 or self.__height == 0:
                     return ("")
-           
+    
                 rect = []
                 for i in range(self.__height):
                     [rect.append(str(self.print_symbol))
-                for j in range(self.__width)]:
-                    if i != self.__height - 1:
+                for j in range(self.__width):
+                     if i != self.__height - 1:
                         rect.append("\n")
                 return ("".join(rect))
 
